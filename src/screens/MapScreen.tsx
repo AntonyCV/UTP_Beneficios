fc50d99784f7c79003b7532660f6158a98755442
AntonyCV <U22200842@utp.edu.pe>
2026-07-17 22:16:05 -0500

    Fix Leaflet marker icons for Render/Docker

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,6 +1,14 @@
-import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
 import { useEffect, useRef } from 'react'
+
+// Fix: Leaflet default icons no se encuentran con bundlers (Vite, webpack)
+delete (L.Icon.Default.prototype as any)._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+})
 import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
 
